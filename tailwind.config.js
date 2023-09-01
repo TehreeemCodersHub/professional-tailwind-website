c0e17d97254c72eaa10d5f308f37c23b3d82eefb
Prosigns <tehreemsaleem001@gmail.com>
2023-09-01 19:05:45 +0500

    dsf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screen: {
+      // we have to define flex for medium or diffrent screens
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily:{
+        'primary': ['Poppins']
+      }
+    },
   },
   plugins: [],
 }
